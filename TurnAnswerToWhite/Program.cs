20776aa5decc582a87094f6071ea1a25ea5fd6b5
Searching96 <nguyenphucthinh2k5@gmail.com>
2024-10-12 19:03:42 +0700

    Add project files.

---
+++ TurnAnswerToWhite/Program.cs
@@ -0,0 +1,37 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"linktoyourdocxfile"; 
+        ChangeCorrectAnswerColor(filePath);
+    }
+
+    static void ChangeCorrectAnswerColor(string filePath)
+    {
+        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filePath, true))
+        {
+            Body body = wordDoc.MainDocumentPart.Document.Body;
+
+            foreach (var paragraph in body.Elements<Paragraph>())
+            {
+                if (paragraph.InnerText.Contains("Đáp án đúng:"))
+                {
+                    foreach (var run in paragraph.Elements<Run>())
+                    {
+                        RunProperties runProperties = new RunProperties();
+                        Color color = new Color() { Val = "FFFFFF" };
+                        runProperties.Append(color);
+                        run.PrependChild(runProperties);
+                    }
+                }
+            }
+
+            wordDoc.MainDocumentPart.Document.Save();
+        }
+    }
+}
